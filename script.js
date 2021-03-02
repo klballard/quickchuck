563cfc7990f8c573d7fae4132018b86f6e2413f8
klballard <kyleleeballard@gmail.com>
2021-03-02 15:05:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const baseURL = 'http://api.icndb.com/jokes/random';
+
+const chuckBtn = document.querySelector('#chuckButton');
+
+chuckBtn.addEventListener('click', findJoke);
+
+async function findJoke() {
+
+    try{
+
+    let response = await fetch(baseURL);
+    let jokeRandom = await response.json();
+    let chosenJoke = jokeRandom.value.joke;
+
+    let jokeResult = document.getElementById('jokeResult');
+    let jokeDisplay = document.createTextNode(`${chosenJoke}`);
+    jokeResult.appendChild(jokeDisplay);
+    
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
